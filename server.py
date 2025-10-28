0c326a976e994ec959660a08e703f460ff49de98
ro-neil <roneilbarrett@gmail.com>
2025-10-28 00:20:16 -0400

    Prepared code for static analysis using PyLint

--- server.py
+++ server.py
@@ -12,14 +12,20 @@ def emotion_analyzer():
     ''' 
     This function accepts text from an HTML GET request and 
     runs emotion analysis over it using the emotion_detector()
-    function. The output returned shows the emotions, their confidence scores and the dominant emotion for the provided text.
+    function. The output returned shows the emotions, their 
+    confidence scores and the dominant emotion for the provided text.
     '''
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-        
-    result = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    result = f"For the given statement, the system response is \
+    'anger': {response['anger']}, \
+    'disgust': {response['disgust']}, \
+    'fear': {response['fear']}, \
+    'joy': {response['joy']} and \
+    'sadness': {response['sadness']}. \
+    The dominant emotion is {response['dominant_emotion']}."
     return result
 
 @app.route("/")
@@ -30,6 +36,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="localhost", port=5000)
